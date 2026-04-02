18cfa99fa52b5c33d3fc7781eea5743d453261c8
phantomrv29-mgl <phantomrv29@gmail.com>
2026-04-02 16:32:15 +0800

    Add score evaluation and feedback to Hello.py

--- Hello.py
+++ Hello.py
@@ -1,3 +1,6 @@
 print("Hello World! My name is Phantom.")
 print("I am 13 years old and I'm learning Python.")
 print(5 + 10)
+score=85
+if score>=90: print("goodjob! you're incredible" )
+else: print("pretty close,try harder next time!")
